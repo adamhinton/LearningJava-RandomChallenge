3a2a0ccfa51be027d529ad02d5a6758d61837fc5
adamd <adam.d.hinton@gmail.com>
2024-04-13 11:23:39 -0500

    boilerplace for dice game

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package dev.lpa;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        List<Integer> currentDice = new ArrayList<>();
     }
+
+    private static void rollDice(List<Integer> currentDice){
+        int randomCount = 5 - currentDice.size();
+
+        var newDice = random
+                .ints(randomCount, 1, 7)
+                .sorted()
+                .boxed()
+                .toList();
+
+        currentDice.addAll(newDice);
+
+        System.out.println("Your dice are " + currentDice);
+    }
+
 }
